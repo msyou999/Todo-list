b2914ae8f30f4e00662d6c751a24d900020ba2c0
msyou999 <null>
2022-04-02 08:26:14 +0000

    feat: todolist css 적용

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,19 @@
 <template>
-  <div class="hello">
-	<a :href=url>Link to Naver!</a>
-	<h2 :align='align'>오른쪽 정렬하기</h2>
-	<h2 :style="{ backgroundColor : bgColor }">노란색</h2>
-	<h1>이름은 {{ object.name }}이고, 나이는 {{ object.age }}살 입니다.</h1>
-	<h3 :class="flag? 'gray':'' ">Class CSS</h3>
-	  
-	<input type="text" v-model="msg"/>
-	<h3 :class="msg">{{ msg }}</h3>
-	  
-	<input type="radio" v-model="radio" value="skyblue">skyblue
-	<input type="radio" v-model="radio" value="gray">gray
-	<input type="radio" v-model="radio" value="pink">pink
-	<p :class="radio">Radio Button</p>
-	  
-	<input type="checkbox" v-model="checkbox" value="skyblue">skyblue
-	<input type="checkbox" v-model="checkbox" value="gray">gray
-	<input type="checkbox" v-model="checkbox" value="pink">pink
-	  
-	<h3 @click="hi">Hello</h3>
-	<h3 @click="bye">Bye</h3>
-	  
-	<ul v-for="item in items">
-		<!-- <li>{{ item.name + "의 가격은 " item.price + "원 입니다." }}</li> -->
-	</ul>
-  </div>
+	<div>
+	</div>
 </template>
 
 <script>
-var code = { name:'MS', age: 24 };
 export default {
   name: 'HelloWorld',
 	data: function() {
 		return {
 		  	msg: '',
-			array: ['자몽', '오렌지', '딸기'],
-			object: code,
-			url: 'https://www.naver.com',
-			align: 'right',
-			bgColor: 'yellow',
-			flag: true,
-			radio: '',
-			items: [
-				{ name:'햄버거', price:'7000' },
-				{ name:'떡볶이', price:'14000' },
-				{ name:'치킨', price:'20000' },
-			],
 		}
-	},
-	computed: {
-		flag2: function() {
-			if(this.msg) return 'gray';
-		}
-	},
-	methods: {
-		hi: function() {
-			alert('안녕하세요');
-		},
-		bye: function() {
-			alert('안녕히 가세요');
-		},
 	},
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-h3 {
-  margin: 40px 0 0;
-}
-a {
-  color: #42b983;
-}
-.gray {
-	background-Color: gray;
-}
-.skyblue {
-	background-Color: skyblue;
-}
-.pink {
-	background-Color: pink;
-}
 </style>
---
+++ src/components/Todolist.vue
@@ -0,0 +1,72 @@
+<template>
+
+	<div>
+		
+		 <!-- main.todo -->
+    <main class="todo">
+        <div class="container">
+            <h1>To - Do List</h1>
+
+            <!-- ul.list_add -->
+            <ul class="list_add">
+                <li class="list_add_tit"><p>등록 하기</p></li>
+                <li><p><input type="text"><button>등 록</button></p></li>
+            </ul>
+            <!--// ul.list_add -->
+
+            <!-- ul.todo_list -->
+            <ul class="todo_list">
+                <li class="todo_list_tit"><p>할 일</p></li>
+
+
+
+                <li class="del_btn">
+                    <p>
+                        <input type="checkbox" name="check1" value="">JS 공부하기
+                    </p>
+                    <ul class="todo_list_btn">
+                        <li><button>수 정</button></li>
+                        <li><button>삭 제</button></li>
+                    </ul>
+                </li>
+
+
+
+                <li>
+                    <p><input type="checkbox" name="check2" value="">웹 알아보기</p>
+                    <p><span></span></p>
+                </li>
+                <li>
+                    <p><input type="checkbox" name="check3" value="">책 읽기</p>
+                    <p><span></span></p>
+                </li>
+                <li>
+                    <p><input type="checkbox" name="check4" value="">거북이한테 전화하기</p>
+                    <p><span></span></p>
+                </li>
+                <li>
+                    <p><input type="checkbox" name="check5" value="">유리랑 소꿉놀이하기</p>
+                    <p><span></span></p>
+                </li>
+            </ul>
+            <!--// ul.todo_list -->
+
+        </div>
+    </main>
+    <!--// main.todo -->
+
+		
+	</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Todolist',
+	data: function() {
+		return {
+		}
+	},
+}
+</script>
+
