0e81a5a08b71d8d626b1db51199cb42f313b25f2
msyou999 <null>
2022-04-02 05:10:48 +0000

    문법 연습

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,85 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+	<a :href=url>Link to Naver!</a>
+	<h2 :align='align'>오른쪽 정렬하기</h2>
+	<h2 :style="{ backgroundColor : bgColor }">노란색</h2>
+	<h1>이름은 {{ object.name }}이고, 나이는 {{ object.age }}살 입니다.</h1>
+	<h3 :class="flag? 'gray':'' ">Class CSS</h3>
+	  
+	<input type="text" v-model="msg"/>
+	<h3 :class="msg">{{ msg }}</h3>
+	  
+	<input type="radio" v-model="radio" value="skyblue">skyblue
+	<input type="radio" v-model="radio" value="gray">gray
+	<input type="radio" v-model="radio" value="pink">pink
+	<p :class="radio">Radio Button</p>
+	  
+	<input type="checkbox" v-model="checkbox" value="skyblue">skyblue
+	<input type="checkbox" v-model="checkbox" value="gray">gray
+	<input type="checkbox" v-model="checkbox" value="pink">pink
+	  
+	<h3 @click="hi">Hello</h3>
+	<h3 @click="bye">Bye</h3>
+	  
+	<ul v-for="item in items">
+		<!-- <li>{{ item.name + "의 가격은 " item.price + "원 입니다." }}</li> -->
+	</ul>
   </div>
 </template>
 
 <script>
+var code = { name:'MS', age: 24 };
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+	data: function() {
+		return {
+		  	msg: '',
+			array: ['자몽', '오렌지', '딸기'],
+			object: code,
+			url: 'https://www.naver.com',
+			align: 'right',
+			bgColor: 'yellow',
+			flag: true,
+			radio: '',
+			items: [
+				{ name:'햄버거', price:'7000' },
+				{ name:'떡볶이', price:'14000' },
+				{ name:'치킨', price:'20000' },
+			],
+		}
+	},
+	computed: {
+		flag2: function() {
+			if(this.msg) return 'gray';
+		}
+	},
+	methods: {
+		hi: function() {
+			alert('안녕하세요');
+		},
+		bye: function() {
+			alert('안녕히 가세요');
+		},
+	},
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
+}
+.gray {
+	background-Color: gray;
+}
+.skyblue {
+	background-Color: skyblue;
+}
+.pink {
+	background-Color: pink;
 }
 </style>
