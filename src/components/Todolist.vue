1c501bab1a1b364d46383bb5a1542233279576d9
msyou999 <null>
2022-04-09 08:33:58 +0000

    feat: 완료 목록 갯수 출력

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -6,6 +6,7 @@
 			<TodoInput @addTodo = "addTodo"> </TodoInput>
             <ul class="todo_list">
                 <li class="todo_list_tit"><p>할 일</p></li>
+				<li><p> {{ remaining }} / {{todolist.length }} 건 처리</p></li>
                 <li class="del_btn" v-for="(item,index) in todolist" :key="index">
                     <p :class="{doneStyle: item.done}">
                         <input type="checkbox" name="check1" v-model="item.done" >{{item.todo}}
@@ -39,6 +40,13 @@ export default {
 			{done: false, todo:"책상 정리하기"},
 		]
 	}
+  },
+  computed: {
+	  remaining() {
+		  return this.todolist.filter(function(val){
+				return val.done;
+		  }).length;
+	  }
   },
   methods: {
 	  addTodo(val){
