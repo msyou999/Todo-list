ae5bdd8e0421425147df0e3e961f69e7dd1024fa
msyou999 <null>
2022-04-09 08:15:31 +0000

    feat: css 변경 & 완료 기능 구현

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,15 +11,15 @@
                 <li>문의사항</li>
             </ul>
             <ul class="foot_bt">
-                <li><p>상호 : 주식회사</p><p>주소 : 광주광역시 다라구 마바사로 ***** 111호 (광주투빌딩)</p></li>
+                <li><p>상호 : 주식회사</p><p>주소 : 서울특별시 성북구 보문로 ***** (서울빌딩)</p></li>
                 <li><p>대표전화 : 1899-****</p><p>사업자등록번호 : 111-11-11111</p></li>
-                <p class="reserved">ⓒ 2022 BULLETINBORD .ALL ***** RESERVED.</p>
+                <p class="reserved">ⓒ 2022 BULLETINBORD .ALL RIGHTS YOO MIN-SEO RESERVED.</p>
             </ul>
             <ul class="foot_icon">
-                <li><a href="#"><img src="img/ico_footer_sns_y.png" alt="footer 유튜브 아이콘"></a></li>
+                <li><a href="#"><img src="/img/ico_footer_sns_y.png" alt="footer 유튜브 아이콘"></a></li>
                 <li><a href="#"><img src="img/ico_footer_sns_i.png" alt="footer 인스타그램 아이콘"></a></li>
                 <li><a href="#"><img src="img/ico_footer_sns_b.png" alt="footer 밴드 아이콘"></a></li>
-                <li><a href="#"><img src="img/ico_footer_sns_gift.png" alt="footer 카카오톡_선물하기 아이콘"></a></li>
+                <li><a href="#"><img src="Todo-list/src/assets/img/ico_footer_sns_gift.png" alt="footer 카카오톡_선물하기 아이콘"></a></li>
             </ul>
         </div>
     </footer>
--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -7,12 +7,12 @@
             <ul class="todo_list">
                 <li class="todo_list_tit"><p>할 일</p></li>
                 <li class="del_btn" v-for="(item,index) in todolist" :key="index">
-                    <p>
-                        <input type="checkbox" name="check1" value="">{{item.todo}}
+                    <p :class="{doneStyle: item.done}">
+                        <input type="checkbox" name="check1" v-model="item.done" >{{item.todo}}
                     </p>
                     <ul class="todo_list_btn">
-                        <li><button>수 정</button></li>
-                        <li><button @click="subTodo">삭 제</button></li>
+                        <!-- <li><button>수 정</button></li> -->
+                        <li><button @click="subTodo(index)">삭 제</button></li>
                     </ul>
                 </li>
             </ul>
@@ -32,23 +32,32 @@ export default {
   data: function() {
 	return {
 		todolist:[
-			{todo:"Vue.js 공부하기"},
-			{todo:"네트워크보안 서브노트 작성하기"},
-			{todo:"실기 강의 듣기"},
-			{todo:"과제 제출하기"},
-			{todo:"책상 정리하기"},
+			{done: false, todo:"Vue.js 공부하기"},
+			{done: false, todo:"네트워크보안 서브노트 작성하기"},
+			{done: false, todo:"실기 강의 듣기"},
+			{done: false, todo:"과제 제출하기"},
+			{done: false, todo:"책상 정리하기"},
 		]
 	}
   },
   methods: {
 	  addTodo(val){
-		  this.todolist.push({todo:val});
+		  this.todolist.push({done: false, todo:val});
 		  this.addText = "";
 	  },
 	  subTodo(idx){
 		  this.todolist.splice(idx, 1);
-	  }
+	  },
+	  // modTodo(idx){
+	  // this.todolist.
+	  // }
   }
 }
 </script>
+<style>
+	.doneStyle {
+		text-decoration: line-through; 
+		color: lightgray;
+	}
+</style>
 
